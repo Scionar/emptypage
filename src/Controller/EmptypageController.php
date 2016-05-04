baabd7fd7b453932dfa3d438783eb53c7569667b
Joona Viertola <joona.viertola@druid.fi>
2016-05-04 13:39:42 +0300

    First commit.

---
+++ src/Controller/EmptypageController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\emptypage\Controller\EmptypageController.
+ */
+
+namespace Drupal\emptypage\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Provides route responses for the Empty module.
+ */
+class EmptypageController extends ControllerBase {
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function content() {
+    $element = array(
+      //'#title' => 'lolo',
+      '#markup' => '',
+    );
+
+    return $element;
+  }
+
+}
